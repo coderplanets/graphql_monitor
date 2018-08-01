ecff12400b283ff9e2061bf9188d753fd8d8ce11
mydearxym <mydearxym@qq.com>
2018-08-01 15:19:20 +0800

    cors debug

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ const launcher = new ApolloEngineLauncher({
       /* parse evn-var issue */
       /* host: process.env.APOLLO_FRONT_HOST, */
       /* port: parseInt(process.env.APOLLO_FRONT_PORT.slice(1, -1)), */
-      port: parseInt(process.env.APOLLO_FRONT_PORT),
+      /* port: parseInt(process.env.APOLLO_FRONT_PORT), */
+      port: 7000, //parseInt(process.env.APOLLO_FRONT_PORT),
       endpoints: ['/graphiql'],
       overrideGraphqlResponseHeaders: {
         'Access-Control-Allow-Origin': '*',
